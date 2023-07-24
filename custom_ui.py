dabbfccb3f6d8cd6aa0582e62cf696754a0a5316
Javed <javedwilde@gmail.com>
2023-07-24 18:36:39 +0530

    push

--- custom_ui.py
+++ custom_ui.py
@@ -15,35 +15,47 @@ class MainApp(App):
     def build(self):
         self.usb_device = None
         self.serial_port = None
+        self.uiDict = {}
         return Builder.load_file('custom_ui.kv')
     
     def on_button_connect(self):
         if platform == 'android':
-            self.usb_device = usb.get_usb_device_list()[0]
-            if not usb.has_usb_permission(self.usb_device):
-                usb.request_usb_permission(self.usb_device)
+            if len(usb.get_usb_device_list())<1:
+                self.uiDict['connecterror'].text = 'No device found, make sure the connections are fine.'
                 return
-            self.serial_port = serial4a.get_serial_port(
-                self.usb_device,
-                115200,
-                8,
-                'N',
-                1,
-                timeout=1
-            )
+            try:
+                self.usb_device = usb.get_usb_device_list()[0]
+                if not usb.has_usb_permission(self.usb_device):
+                    usb.request_usb_permission(self.usb_device)
+                    return
+                self.serial_port = serial4a.get_serial_port(
+                    self.usb_device,
+                    115200,
+                    8,
+                    'N',
+                    1,
+                    timeout=1
+                )
+            except Exception as e:
+                self.uiDict['connecterror'].text = str(e)
         else:
-            self.usb_device = list_ports.comports()[0].device
-            self.serial_port = Serial(
-                self.usb_device,
-                115200,
-                8,
-                'N',
-                1,
-                timeout=1
-            )
-        
-        print(self.serial_port)
-    
+            if len(list_ports.comports())<2:
+                self.uiDict['connecterror'].text = 'No device found, make sure the connections are fine.'
+                return
+            
+            try:
+                self.usb_device = list_ports.comports()[0].device
+                self.serial_port = Serial(
+                    self.usb_device,
+                    115200,
+                    8,
+                    'N',
+                    1,
+                    timeout=1
+                )
+            except Exception as e:
+                self.uiDict['connecterror'].text = str(e)
+            print(self.serial_port)
 
 if __name__ == '__main__':
     MainApp().run()
